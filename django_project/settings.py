b858c7305ad537f0992e08ce69397f636db13b9e
Vishnu <vishnuak15@gmail.com>
2021-09-21 12:32:46 +0530

    finallllllllyyy

--- django_project/settings.py
+++ django_project/settings.py
@@ -169,5 +169,6 @@ AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
 AWS_S3_FILE_OVERWRITE = False
-AWD_DEFAULT_ACL = None
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S#Boto3Storage'
+AWS_DEFAULT_ACL = None
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
